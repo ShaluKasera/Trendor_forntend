4df670e025f2ce987caecb9c1cae3606cc301fe7
ShaluKasera <shalukumari93129@gmail.com>
2025-06-17 22:09:16 +0530

    setup reactjs and other dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-center'>
+ hello world
+    </div>
+    
+    </>
+  )
+}
+
+export default App
